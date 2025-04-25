0250ea1b728206e76c648582fbb685a741518b5d
Not a cow <kly3333aswd@gmail.com>
2025-04-24 23:12:52 -0700

    update navbar

--- src/components/header/header.svelte
+++ src/components/header/header.svelte
@@ -1,22 +1,27 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
-	import { PUBLIC_BOT_NAME, PUBLIC_GUILD_INVITE, PUBLIC_BOT_INVITE } from '$env/static/public';
+	import {
+		PUBLIC_BOT_NAME,
+		PUBLIC_GUILD_INVITE,
+		PUBLIC_BOT_INVITE,
+		PUBLIC_GITHUB_USERNAME,
+		PUBLIC_GITHUB_REPO
+	} from '$env/static/public';
 	import './style.scss';
 
 	const navLinks = [
 		{ name: 'Home', url: '/' },
-		{ name: 'Features', url: 'features' },
-		{ name: 'Commands', url: 'commands' },
+		{ name: 'Dashboard', url: 'dashboard' },
 		{ name: 'Documentation', url: 'docs' },
-		{ name: 'Support', url: PUBLIC_GUILD_INVITE },
-		{ name: 'Invite', url: PUBLIC_BOT_INVITE }
+		{ name: 'Source', url: `https://github.com/${PUBLIC_GITHUB_USERNAME}/${PUBLIC_GITHUB_REPO}` },
+		{ name: 'Support', url: PUBLIC_GUILD_INVITE }
 	];
 </script>
 
 <header>
 	<div class="container">
 		<div class="logo">
-			<span class="bot-icon">🤖</span>
+			<Icon icon="twemoji:cow-face" />
 			<h2>{PUBLIC_BOT_NAME}</h2>
 		</div>
 
