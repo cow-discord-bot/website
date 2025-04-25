ecf244cd23cc2a706ed77ede9c605421d74a79ba
Not a cow <kly3333aswd@gmail.com>
2025-04-25 00:36:47 -0700

    fix mobile nav

--- src/components/header/header.svelte
+++ src/components/header/header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
+	import { onMount } from 'svelte';
 	import {
 		PUBLIC_BOT_NAME,
 		PUBLIC_GUILD_INVITE,
@@ -15,6 +16,16 @@
 		{ name: 'Source', url: `https://github.com/${PUBLIC_GITHUB_USERNAME}/${PUBLIC_GITHUB_REPO}` },
 		{ name: 'Support', url: PUBLIC_GUILD_INVITE }
 	];
+
+	let mobileMenuOpen = false;
+
+	function toggleMobileMenu() {
+		mobileMenuOpen = !mobileMenuOpen;
+	}
+
+	function handleLinkClick() {
+		mobileMenuOpen = false;
+	}
 </script>
 
 <header>
@@ -24,10 +35,14 @@
 			<h2>{PUBLIC_BOT_NAME}</h2>
 		</div>
 
-		<nav>
+		<div class="mobile-menu-button" on:click={toggleMobileMenu}>
+			<Icon icon={mobileMenuOpen ? 'mdi:close' : 'mdi:menu'} width="24" height="24" />
+		</div>
+
+		<nav class:active={mobileMenuOpen}>
 			<ul>
 				{#each navLinks as link}
-					<li><a href={link.url}>{link.name}</a></li>
+					<li><a href={link.url} on:click={handleLinkClick}>{link.name}</a></li>
 				{/each}
 			</ul>
 		</nav>
