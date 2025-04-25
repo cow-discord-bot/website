5d32ead6cac605424744ec867152662217e36595
Not a cow <kly3333aswd@gmail.com>
2025-04-24 23:12:46 -0700

    remove unused stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,6 @@
 <script>
 	import Header from '$comp/header/header.svelte';
 	import '../app.css';
-
-	const botName = 'ModGuard';
-
-	const navLinks = [
-		{ name: 'Home', url: '#' },
-		{ name: 'Features', url: '#features' },
-		{ name: 'Commands', url: '#commands' },
-		{ name: 'Documentation', url: '#docs' },
-		{ name: 'Support', url: '#support' }
-	];
 </script>
 
 <div class="app">
